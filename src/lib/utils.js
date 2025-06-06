1a9d0eb7bd196ff9261c744a7bfd68ed55f410bd
Murad Gurbanzada <gurbanzadamurad@gmail.com>
2025-06-06 13:56:41 +0400

    asdas

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -13,7 +13,7 @@ export const generateToken = (userId, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // MS
       httpOnly: true, // prevent XSS attacks cross-site request forgery attacks
       path: "/",
-      ...(development !== "development" && {
+      ...(development === "development" && {
          sameSite: "none",
          domain: NODE_BACKEND, // replace with your domain
          secure: true // use secure cookies in production
